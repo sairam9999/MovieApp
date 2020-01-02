78509cde6e99759cce6e1b68cb0117799526c017
sairam9999 <vsairamanroid@gmail.com>
2020-01-01 22:38:33 -0500

    Retrieved JOSN Objects

--- app/src/main/java/com/example/movieapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/movieapp/Activities/MainActivity.java
@@ -1,20 +1,45 @@
 package com.example.movieapp.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.movieapp.Data.MovieRecyclerViewAdapter;
+import com.example.movieapp.Model.Movie;
 import com.example.movieapp.R;
+import com.example.movieapp.Util.Constants;
+import com.example.movieapp.Util.Prefs;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
+    private List<Movie> movieList;
+    private RequestQueue queue;
 
 
     @Override
@@ -24,14 +49,65 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        queue = Volley.newRequestQueue(this);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movieList = new ArrayList<>();
+        Prefs prefs = new Prefs(MainActivity.this);
+        String search = prefs.getSearch();
+        getMovies(search);
+
+    }
+
+//    Get Movies
+    public List<Movie> getMovies(String searchTerm){
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Constants.URL_LEFT
+                + searchTerm + Constants.URL_RIGHT, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try{
+                    JSONArray moviesArray = response.getJSONArray("Search");
+
+                    for(int i = 0; i < moviesArray.length(); i++){
+                        JSONObject movieObj = moviesArray.getJSONObject(i);
+                        Movie movie = new Movie();
+                        movie.setTitle(movieObj.getString("Title"));
+                        movie.setYear(movieObj.getString("Year"));
+                        movie.setMovieType(movieObj.getString("Type"));
+                        movie.setPoster(movieObj.getString("Poster"));
+                        movie.setImdbId(movieObj.getString("imdbID"));
+
+                        Log.d("Movies: ", movie.getTitle());
+                    }
+
+                }catch (JSONException e){
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+        return movieList;
     }
 
     @Override
---
+++ app/src/main/java/com/example/movieapp/Util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.movieapp.Util;
+
+public class Constants {
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s";
+    public static final String URL_RIGHT = "&page=2";
+
+}
